c607deca1b43c53c3d3dd82ac80b957d39f32d8e
anderson <andersondinizep@gmail.com>
2024-06-27 10:49:42 -0300

    First commit

---
+++ components/Hero.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from 'react'
+
+import Image from 'next/image';
+
+import { CustomButton } from '.';
+const Hero = () => {
+  const handleScroll = () => {
+
+  }
+  return (
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        <h1 className='hero__title'>
+          Encontre, reserve ou alugue um carro com rapidez e facilidade!
+        </h1>
+        <p className='hero__subtitle'>
+          Agilize sua experiência de aluguel de carro com nosso processo de reserva fácil.
+        </p>
+        <CustomButton
+          title="explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className='hero__image-overlay'>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero;
---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
