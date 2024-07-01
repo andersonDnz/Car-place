a49201d1231f556321e6ff6a4088b6dca2005859
anderson <andersondinizep@gmail.com>
2024-07-01 19:58:51 -0300

    Fix:Ajustando componente card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 
 import { calculateCarRent } from '@/utils';
+
 import Image from 'next/image';
+import { CarProps } from '@/types';
+import { CustomButton } from '.';
+
+interface CarCardProps {
+  car: CarProps;
+}
 
 
-const CarCard = ({ car }: carCardPorps) => {
+const CarCard = ({ car }: CarCardProps) => {
+
   const { city_mpg, year, make, model, transmission, drive } = car;
 
   const carRent = calculateCarRent(city_mpg, year);
@@ -25,16 +33,13 @@ const CarCard = ({ car }: carCardPorps) => {
         <span className='self-end text-[14px] font-medium'>
           /dia
         </span>
-        <div className='relative w-full h-40 my-3 object-contain'>
-          <Image
-            src="/hero.png"
-            alt="modelo de carro"
-            width={50}
-            height={50}
-            className='object-contain' />
-
-        </div>
       </p>
+      <div className='relative w-full h-40 my-3 object-contain'>
+        <Image
+          src="/hero.png"
+          alt="modelo de carro" fill priority
+          className='object-contain' />
+      </div>
     </div>
   )
 }
--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,4 @@
+import { CarCard } from "@/components";
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
@@ -11,4 +12,19 @@ export interface CustomButtonProps {
 export interface SearchManufacturerProps {
   manufacturer: string;
   setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
