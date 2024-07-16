9799d2b9e9f863b4ef1643304cdb84a3e845a57a
anderson <andersondinizep@gmail.com>
2024-07-16 13:55:06 -0300

    Pequenas alterações

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,14 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     'x-rapidapi-key': 'd64c24e974msh1b540dd739731cfp17cf45jsn5d6be5acb5e1',
     'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer} &year=${year} &model=${model} &limit=${limit}&fuel_type=${fuel}`, {
     headers: headers,
   });
 
